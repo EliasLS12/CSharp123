1c82914f8f16cffa32b76039b27f46c01ab9cc71
EliasLS12 <114383553+EliasLS12@users.noreply.github.com>
2022-09-28 22:08:15 +0200

    Add files via upload

---
+++ inventory.cs
@@ -0,0 +1,27 @@
+using System; 
+ 
+ 
+ 
+ 
+    class Inventory 
+    { 
+             
+        public void inventory(Programet _program, string _valg) 
+        { 
+            switch (_valg) 
+            { 
+                case "List": 
+                    for(int i = 1; i < 13; i++) 
+                    { 
+                         
+                        Console.WriteLine(_program.getcar(i).completeCarspec()); 
+                    } 
+                    break; 
+                default: 
+                    Console.WriteLine("Input not recognised"); 
+                    break; 
+                 
+            } 
+        
+        } 
+    } 
